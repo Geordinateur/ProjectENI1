d657524c8be363c0c749508da8f469e43bc7846c
laurent <laurent.richir@grow-your-business.fr>
2023-07-24 15:52:29 +0200

    starter springBoot

---
+++ src/main/java/fr/eni/projecteni1/ProjectEni1Application.java
@@ -0,0 +1,13 @@
+package fr.eni.projecteni1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEni1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProjectEni1Application.class, args);
+  }
+
+}
---
+++ src/test/java/fr/eni/projecteni1/ProjectEni1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.projecteni1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEni1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
